abcff5fcc702f0197e58d37e5834d3ecacc2aafd
shivamdaksh404 <shivofficial05@gmail.com>
2023-05-03 16:23:17 +0530

    routing updated

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { linkData } from '../utils/data'
 import { Link } from 'react-router-dom'
+import style from './NavigationBar.module.css'
+// import Button from '@mui/material/Button'
 
 export default function NavigationBar() {
 
@@ -9,12 +11,13 @@ export default function NavigationBar() {
 
 
   return (
-    <div>
-    <div>
-      <img src="" alt="" />
+    <div className={style.mainContainer}>
+      <img src="/logo.png"  height={100} width={150} alt="" />
+    <div className={style.linksWrapper}>
       {linkData.map((ele,index)=> <li key={index}>
-        <Link to={ele.link} >{ele.linkName}</Link>
+        <Link to={ele.path} >{ele.linkName}</Link>
       </li>)}
+      {/* <Button variant="contained">Join Us</Button> */}
     </div>
 
 
