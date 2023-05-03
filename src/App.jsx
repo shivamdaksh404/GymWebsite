f804f7fdd783fe461167dcfa6a914a88e02157a3
shivamdaksh404 <shivofficial05@gmail.com>
2023-05-03 15:19:37 +0530

    folder structure Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 function App() {
+  return <div>
 
-  return (
-    <div className="App">
-      </div>
-  )
+<Routes>
+<Route path="/" element={<Home/>}  />
+<Route path="/aboutUs" element={<AboutUs/>}  />
+
+</Routes>
+
+  </div>;
 }
 
-export default App
+export default App;
